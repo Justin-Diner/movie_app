b7ea422bed8097f370f1f9800bc6694e6d9ee3c3
Justin <90215009+DinerJ@users.noreply.github.com>
2022-06-16 22:35:09 -0400

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,34 @@
+//import React from 'react';
+//import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavbarElements';
+
+//const Navbar = () => {
+//	return (
+//		<>
+//			<Nav>
+//				<NavLink to="/">
+//					<img src="" alt=""/> 
+//				</NavLink>	
+//				<Bars />
+//				<NavMenu>
+//					<NavLink to="/about" activeStyle>
+//						About
+//					</NavLink>
+//					<NavLink to="/about" activeStyle>
+//						Services
+//					</NavLink>
+//					<NavLink to="/about" activeStyle>
+//						Contact
+//					</NavLink>
+//					<NavLink to="/about" activeStyle>
+//						Sign Up
+//					</NavLink>
+//				</NavMenu>
+//				<NavBtn>
+//					<NavBtnLink to='signin'>Sign In</NavBtnLink>
+//				</NavBtn>
+//			</Nav>
+//		</>
+//	);
+//};
+
+//export default Navbar;
